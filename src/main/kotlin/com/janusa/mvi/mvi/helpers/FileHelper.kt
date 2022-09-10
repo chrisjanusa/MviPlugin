7f7cbd4deca58f8930526b2d06fe962def24ce4e
chrisjanusa <chrisjanusa@gmail.com>
2022-09-10 13:07:39 -0700

    Add data result class

--- src/main/kotlin/com/janusa/mvi/mvi/helpers/FileHelper.kt
+++ src/main/kotlin/com/janusa/mvi/mvi/helpers/FileHelper.kt
@@ -3,44 +3,15 @@ package com.janusa.mvi.mvi.helpers
 import com.intellij.openapi.actionSystem.AnActionEvent
 import com.intellij.openapi.actionSystem.PlatformDataKeys
 import com.intellij.openapi.application.WriteAction
+import com.intellij.openapi.editor.impl.EditorImpl
 import com.intellij.openapi.fileTypes.FileType
-import com.intellij.openapi.vfs.VirtualFile
 import com.intellij.psi.PsiDirectory
 import com.intellij.psi.PsiElement
 import com.intellij.psi.PsiFileFactory
-import com.intellij.psi.PsiManager
 import com.intellij.util.ThrowableRunnable
-import com.janusa.mvi.mvi.base.BasePathService
+import com.janusa.mvi.mvi.screen.stateFileSuffix
 import org.jetbrains.kotlin.idea.KotlinIconProviderService
 import javax.swing.Icon
-
-
-fun isDirectory(event: AnActionEvent): Boolean {
-    val virtualFile = event.getData(PlatformDataKeys.VIRTUAL_FILE) ?: return false
-    return virtualFile.isDirectory && virtualFile.parent.parent.path.contains("main") && !virtualFile.path.contains("res")
-}
-
-fun createDirectory(event: AnActionEvent, name: String) {
-    val parentDirectory = event.getData(PlatformDataKeys.PSI_ELEMENT) as? PsiDirectory ?: return
-    if (parentDirectory.findSubdirectory(name) == null) {
-        WriteAction.run(ThrowableRunnable { parentDirectory.createSubdirectory(name) })
-    }
-}
-
-fun createSubDirectory(parentDirectory: PsiDirectory, name: String) {
-    if (parentDirectory.findSubdirectory(name) == null) {
-        WriteAction.run(ThrowableRunnable { parentDirectory.createSubdirectory(name) })
-    }
-}
-
-fun getDirectory(event: AnActionEvent, name: String): PsiDirectory? {
-    val parentDirectory = event.getData(PlatformDataKeys.PSI_ELEMENT) as? PsiDirectory ?: return null
-    return parentDirectory.findSubdirectory(name)
-}
-
-fun getDirectoryFromParent(parentDirectory: PsiDirectory, name: String): PsiDirectory? {
-    return parentDirectory.findSubdirectory(name)
-}
 
 fun createFile(event: AnActionEvent, directory: PsiDirectory, template: FileTemplate) {
     if (directory.findFile("${template.title}.kt") != null) return
@@ -54,6 +25,29 @@ fun createFile(event: AnActionEvent, directory: PsiDirectory, template: FileTemp
     })
 }
 
+open class FileTemplate(val title: String, val content: String)
+
+fun PsiElement.getFileName() = this.containingFile?.name
+
+fun PsiElement.getFileText() = this.containingFile?.text
+
+fun getStateValue(event: AnActionEvent): StateValue? {
+    val value = event.getData(PlatformDataKeys.EDITOR) ?: return null
+    val document = value.document
+    if (!(value as EditorImpl).virtualFile.name.contains(stateFileSuffix)) return null
+    val position = value.caretModel.currentCaret.visualPosition
+    val valueLine = document.text.split("\n")[position.line].substringAfter("val ", "")
+    val name = valueLine.substringBefore(":", "")
+    val type = valueLine.substringAfter(": ", "").substringBefore(" ")
+    if (name.isBlank() || type.isBlank()) return null
+    return StateValue(name, type)
+}
+
+data class StateValue(
+    val name: String,
+    val type: String
+)
+
 object KotlinFileType : FileType {
     override fun getName() = "Kotlin"
 
@@ -64,55 +58,4 @@ object KotlinFileType : FileType {
     override fun getIcon(): Icon = KotlinIconProviderService.getInstance().fileIcon
 
     override fun isBinary() = false
-}
-
-open class FileTemplate(val title: String, val content: String)
-
-fun PsiDirectory.getPackage() = virtualFile.getPackage()
-
-fun VirtualFile.getPackage(): String {
-    return path.pathToPackage()
-}
-
-fun String.pathToPackage(): String {
-    return this.substringAfter("main/").substringAfter("/").replace('/', '.')
-}
-
-fun PsiElement.getFileName() = this.containingFile?.name
-
-fun PsiElement.getFileText() = this.containingFile?.text
-
-private fun getBasePath(event: AnActionEvent): String? {
-    val project = event.getData(PlatformDataKeys.PROJECT) ?: return null
-    return BasePathService.getService(project).basePath
-}
-
-fun getBasePackage(event: AnActionEvent): String {
-    return getBasePath(event)?.pathToPackage() ?: ""
-}
-
-fun saveBasePath(event: AnActionEvent, path: String) {
-    val project = event.getData(PlatformDataKeys.PROJECT) ?: return
-    BasePathService.getService(project).basePath = path
-}
-
-
-fun getBase(event: AnActionEvent): String? {
-    val virtualFile = event.getData(PlatformDataKeys.VIRTUAL_FILE) ?: return null
-    val basePathStored = getBasePath(event)
-    if (!basePathStored.isNullOrBlank()) {
-        return basePathStored
-    }
-    if (virtualFile.parent.name == "base") {
-        return virtualFile.parent.path
-    }
-    return null
-}
-
-fun getBaseDirectory(event: AnActionEvent): PsiDirectory? {
-    val basePath = getBase(event) ?: return null
-    val virtualFile = event.getData(PlatformDataKeys.VIRTUAL_FILE) ?: return null
-    val virtualBaseDir = virtualFile.fileSystem.findFileByPath(basePath) ?: return null
-    val project = event.getData(PlatformDataKeys.PROJECT) ?: return null
-    return PsiManager.getInstance(project).findDirectory(virtualBaseDir)
 }
